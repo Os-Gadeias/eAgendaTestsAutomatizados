e977c254331408de0a685d63fb3d038bcaeb251e
thig0S_ <thiagokovalski5@gmail.com>
2026-08-06 15:56:09 -0300

    add todos os projects dos testes bases

---
+++ test/Eagenda.Test.Integracao/Test1.cs
@@ -0,0 +1,10 @@
+﻿namespace Eagenda.Test.Integracao;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
---
+++ test/Eagenda.Test.Unitario/Test1.cs
@@ -0,0 +1,10 @@
+﻿namespace Eagenda.Test.Unitario;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
