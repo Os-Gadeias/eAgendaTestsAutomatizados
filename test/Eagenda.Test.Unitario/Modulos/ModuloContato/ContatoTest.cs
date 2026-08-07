a3a691f62228de72570e6195f3a882e4a27c0acf
thig0S_ <thiagokovalski5@gmail.com>
2026-08-07 17:35:43 -0300

    CT-CON-002 implementado

--- test/Eagenda.Test.Unitario/Modulos/ModuloContato/ContatoTest.cs
+++ test/Eagenda.Test.Unitario/Modulos/ModuloContato/ContatoTest.cs
@@ -14,4 +14,13 @@ public class ContatoTest
 
         Assert.HasCount(0, erros);
     }
+    [TestMethod]
+    public void Cadastrar_contato_apenas_com_os_campos_obrigatórios()
+    {
+        Contato contato = new("Thiago Kovalski", "Thiago@gmail.com", "(49) 98888-8888", null, null);
+
+        List<string> erros = contato.Validar();
+
+        Assert.HasCount(0, erros);
+    }
 }
--- test/Eagenda.Test.Unitario/Modulos/ModuloContato/ServicoContatoTest.cs
+++ test/Eagenda.Test.Unitario/Modulos/ModuloContato/ServicoContatoTest.cs
@@ -30,4 +30,25 @@ public class ServicoContatoTest
         Assert.IsNotNull(contatoCadastrado);
         repositorioContato.Verify(r => r.Cadastrar(It.IsAny<Contato>()), Times.Once);
     }
+    [TestMethod]
+    public void Cadastrar_contato_apenas_com_os_campos_obrigatórios()
+    {
+        Mock<IRepositorioContato> repositorioContato = new();
+        Mock<IRepositorioCompromisso> repositorioCompromisso = new();
+
+        repositorioContato.Setup(r => r.SelecionarTodos()).Returns([]);
+
+        ServicoContato servicoContato = new(repositorioContato.Object, repositorioCompromisso.Object);
+
+        Contato? contatoCadastrado = null!;
+
+        repositorioContato.Setup(r => r.Cadastrar(It.IsAny<Contato>())).Callback<Contato>(c => contatoCadastrado = c);
+
+        Result resultado = servicoContato.Cadastrar(new("Thiago Kovalski", "Thiago@gmail.com", "(49) 98888-8888", null, null));
+
+
+        Assert.IsTrue(resultado.IsSuccess);
+        Assert.IsNotNull(contatoCadastrado);
+        repositorioContato.Verify(r => r.Cadastrar(It.IsAny<Contato>()), Times.Once);
+    }
 }
