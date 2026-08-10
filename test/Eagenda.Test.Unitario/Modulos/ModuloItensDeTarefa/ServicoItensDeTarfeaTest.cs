a43e6b09aa2960ce99d633df9b7ea800e0b099dc
thig0S_ <thiagokovalski5@gmail.com>
2026-08-10 11:09:58 -0300

    CT-ITT-005add

--- test/Eagenda.Test.Unitario/Modulos/ModuloItensDeTarefa/ServicoItensDeTarfeaTest.cs
+++ test/Eagenda.Test.Unitario/Modulos/ModuloItensDeTarefa/ServicoItensDeTarfeaTest.cs
@@ -46,4 +46,22 @@ public class ServicoItensDeTarfeaTest
         Assert.Contains("O campo \"Título\" do Item Tarefa é obrigatório!", resultado.Errors.First().Message);
         repositorioTarefa.Verify(r => r.Editar(It.IsAny<Guid>(), It.IsAny<Tarefa>()), Times.Never);
     }
+    [TestMethod]
+    public void AdicionarItem_SemTarefa_Vinculada_RetornaErro()
+    {
+        Mock<IRepositorioTarefa> repositorioTarefa = new();
+
+        ServicoTarefa servicoTarefa = new(repositorioTarefa.Object);
+
+        Tarefa tarefa = new("lavar o cachorro", PrioridadeTarefa.Alta);
+
+        repositorioTarefa.Setup(r => r.SelecionarPorId(It.IsAny<Guid>())).Returns<Tarefa?>(null!);
+        repositorioTarefa.Setup(r => r.Editar(It.IsAny<Guid>(), It.IsAny<Tarefa>())).Returns(false);
+
+        Result resultado = servicoTarefa.AdicionarItem(new(tarefa.Id, string.Empty));
+
+        Assert.IsTrue(resultado.IsFailed);
+        Assert.Contains("Tarefa não encontrada.", resultado.Errors.First().Message);
+        repositorioTarefa.Verify(r => r.Editar(It.IsAny<Guid>(), It.IsAny<Tarefa>()), Times.Never);
+    }
 }
